207ddb4cb717647db6c719efcdf3f1a23ef025b4
jinoosss <jwchoi@onbloc.xyz>
2024-04-01 07:11:08 +0900

    chore: Separate Graphql schemas

--- serve/graph/setup.go
+++ serve/graph/setup.go
@@ -4,14 +4,15 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/go-chi/chi/v5"
-
 	"github.com/gnolang/tx-indexer/events"
+	"github.com/gnolang/tx-indexer/serve/graph/generated"
+	"github.com/gnolang/tx-indexer/serve/graph/resolvers"
 	"github.com/gnolang/tx-indexer/storage"
+	"github.com/go-chi/chi/v5"
 )
 
 func Setup(s storage.Storage, manager *events.Manager, m *chi.Mux) *chi.Mux {
-	srv := handler.NewDefaultServer(NewExecutableSchema(Config{Resolvers: NewResolver(s, manager)}))
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: resolvers.NewResolver(s, manager)}))
 
 	srv.AddTransport(&transport.Websocket{})
 
