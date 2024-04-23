04254562fff098eb11110aca22c61050f79d4f84
jinoosss <112360739+jinoosss@users.noreply.github.com>
2024-04-23 19:31:00 +0900

    Update serve/graph/model/transaction.go
    Co-authored-by: Antonio Navarro Perez <antnavper@gmail.com>

--- serve/graph/model/transaction.go
+++ serve/graph/model/transaction.go
@@ -51,8 +51,8 @@ func (t *Transaction) Success() bool {
 	return t.txResult.Response.IsOK()
 }
 
-func (t *Transaction) Response() TransactionResponse {
-	return TransactionResponse{response: t.txResult.Response}
+func (t *Transaction) Response() *TransactionResponse {
+	return &TransactionResponse{response: t.txResult.Response}
 }
 
 func (t *Transaction) GasWanted() int {
