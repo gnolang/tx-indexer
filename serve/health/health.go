d762023ac7a439aa1fda2d4c1ea530ba308d0af9
Antonio Navarro Perez <antnavper@gmail.com>
2025-08-30 08:46:58 +0200

    feat(api): Add a `/ready` endpoint (#192)
    * Add a /ready endpoint
    
    This helps to know when the node is on sync with the data on the chain.
    
    Changed the /health response to have the same format as the /ready one.
    
    Signed-off-by: Antonio Navarro Perez <antnavper@gmail.com>
    
    * linter
    
    Signed-off-by: Antonio Navarro Perez <antnavper@gmail.com>
    
    * linter field alignment
    
    Signed-off-by: Antonio Navarro Perez <antnavper@gmail.com>
    
    * linter line lenght
    
    Signed-off-by: Antonio Navarro Perez <antnavper@gmail.com>
    
    ---------
    
    Signed-off-by: Antonio Navarro Perez <antnavper@gmail.com>

--- serve/health/health.go
+++ serve/health/health.go
@@ -11,20 +11,61 @@ import (
 	"github.com/gnolang/tx-indexer/storage"
 )
 
-func Setup(s storage.Storage, m *chi.Mux) *chi.Mux {
+func Setup(s storage.Storage, rc ReadyChecker, m *chi.Mux) *chi.Mux {
 	m.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		h, err := s.GetLatestHeight()
 		if err != nil {
-			fmt.Fprintf(w, "ERROR: %s\n", err)
+			render.JSON(w, r, &response{
+				Message: fmt.Sprintf("storage is not reachable: %s", err.Error()),
+				Info: map[string]any{
+					"time": time.Now().String(),
+				},
+			})
+
 			render.Status(r, http.StatusInternalServerError)
 
 			return
 		}
 
-		fmt.Fprintf(w, "Server is responding\n")
-		fmt.Fprintf(w, "- Time: %s\n", time.Now())
-		fmt.Fprintf(w, "- Latest Height: %d\n", h)
+		render.JSON(w, r, &response{
+			Message: "Server is responding",
+			Info: map[string]any{
+				"time":   time.Now().String(),
+				"height": h,
+			},
+		})
+	})
+
+	m.HandleFunc("/ready", func(w http.ResponseWriter, r *http.Request) {
+		ok, err := rc.IsReady()
+		if !ok {
+			render.JSON(w, r, &response{
+				Message: fmt.Sprintf("node not ready: %s", err.Error()),
+				Info: map[string]any{
+					"time": time.Now().String(),
+				},
+			})
+			render.Status(r, http.StatusInternalServerError)
+
+			return
+		}
+
+		render.JSON(w, r, &response{
+			Message: "node is ready",
+			Info: map[string]any{
+				"time": time.Now().String(),
+			},
+		})
 	})
 
 	return m
+}
+
+type response struct {
+	Info    map[string]any `json:"info"`
+	Message string         `json:"message"`
+}
+
+type ReadyChecker interface {
+	IsReady() (bool, error)
 }
